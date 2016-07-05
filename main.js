36e76a340660df99d50ea4c58c1d3cf8fa561c6c
hwagz <hunt.wagner@gmail.com>
2016-07-05 12:46:39 -0400

    Added key handlers for axis adjust

--- main.js
+++ main.js
@@ -19,14 +19,21 @@ angular
 
     $scope.cube = new THREE.Mesh($scope.cubeGeometry,$scope.cubeMaterial);
 
+    $scope.cube2 = new THREE.Mesh($scope.cubeGeometry,$scope.cubeMaterial);
+
     $scope.cube.position.x = 10;
     $scope.cube.position.y = 10;
     $scope.cube.position.z = 10;
 
+    $scope.cube2.position.x = -10;
+    $scope.cube2.position.y = 10;
+    $scope.cube2.position.z = 20;
+
     $scope.scene.add($scope.cube);
+    $scope.scene.add($scope.cube2);
 
     $scope.camera.position.x = 40;
-    $scope.camera.position.y = 40;
+    $scope.camera.position.y = 25;
     $scope.camera.position.z = 40;
 
     $scope.camera.lookAt($scope.scene.position);
@@ -37,17 +44,41 @@ angular
       requestAnimationFrame( render );
       $scope.cube.rotation.x += 0.05;
       $scope.cube.rotation.y += 0.05;
+      $scope.cube2.rotation.x += 0.05;
+      $scope.cube2.rotation.y += 0.05;
       $scope.renderer.render( $scope.scene, $scope.camera );
     }
     render();
 
+
     $scope.cameraAdjust = function(axis,positive){
-      var amount = -1;
+      var amount = -5;
       if(positive){
           amount *= -1;
       }
       $scope.camera.position[axis]+=amount
       $scope.camera.lookAt($scope.scene.position);
+    };
+    // Handles key presses
+    $scope.keyHandler = function(e){
+      e = e.keyCode;
+      //console.log(e);
+      //w: 87 s: 83, y axis
+      //a: 65 d: 68, x axis
+      //q: 81 e: 69, z axis
+
+      if(e>=65 && e<=87){
+        var axis = 'y';
+        var positive = false;
+        if(e<83){
+          axis = e<69 ? 'x' : 'z';
+        }
+        if(e==87 || e==65 || e==81){
+          positive = true;
+        }
+        $scope.cameraAdjust(axis,positive);
+      }
+
     };
 
 
